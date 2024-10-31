9944cd6ff54559f9e08f518e296fb2291e45f73d
Freyaui <freya.sostack@gmail.com>
2024-10-31 15:22:04 +0100

    ???

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@ import { RouterView } from 'vue-router';
 import BookingView from './views/BookingView.vue';
 import EventView from './views/EventView.vue';
 import InfoView from './views/InfoView.vue';
+import EventCarousel from './components/EventCarousel.vue';
 </script>
 
 <template>
-    <RouterView />
-    <EventView />
+    <header>
+        <RouterView />
+    </header>
+
+    <main>
+        <EventView />
+        <EventCarousel />
+        <InfoView />
+        <BookingView />
+    </main>
 </template>
